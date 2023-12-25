217995cc754c85cf24d95133eb18374380638957
pradhuman29 <154812784+pradhuman29@users.noreply.github.com>
2023-12-26 00:53:38 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ImageUploader from './components/FirstComponent'; // Import your ImageUploader component
-import SecondComponent from './components/SecondComponent'; // Import your SecondComponent
+import ImageUploader from './components/FirstComponent';
+import SecondComponent from './components/SecondComponent'; 
 
 const App = () => {
   return (
